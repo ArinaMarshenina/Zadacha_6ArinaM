bb99ace2f7ace16d482ebc1be48e6cdb9e13d14b
Arina M <apelsinka7.ru@yandex.ru>
2022-09-20 22:14:00 +0300

    задача 6 решение

---
+++ Zadacha6_ArinaM/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число: ");
+int a;                             
+a = Convert.ToInt32(Console.ReadLine()); 
+ 
+if (a % 2 == 0)
+{
+    Console.WriteLine("четное число");
+           
+}
+ else
+{
+    Console.WriteLine("нечетное"); //
+};
